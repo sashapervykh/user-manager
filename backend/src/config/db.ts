6a38a8b2d3cb2d4ba33b1bfe838b06c2d8fbcbe9
Aleksandr Pervykh <alekspervykh2230521@gmail.com>
2026-01-31 12:34:04 +0300

    feat: add typescript to project and files

--- backend/src/config/db.ts
+++ backend/src/config/db.ts
@@ -5,19 +5,18 @@ dotenv.config();
 
 const pool = new Pool({
   host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
+  port: Number(process.env.DB_PORT),
   database: process.env.DB_NAME,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
 });
 
-pool.connect((err, client, release) => {
-  if (err) {
-    console.error("Database connection failed", err.stack);
-  } else {
-    console.log("Database connected successfully");
-    release();
-  }
-});
+try {
+  const client = await pool.connect();
+  console.log("Database connected successfully");
+  client.release();
+} catch {
+  console.error("");
+}
 
 export default pool;
--- backend/src/routes/auth.route.ts
+++ backend/src/routes/auth.route.ts
@@ -1,6 +1,6 @@
 import { Router } from "express";
 
-const authRouter = new Router();
+const authRouter = Router();
 
 authRouter.post("/register", (req, res) => {
   res.status(501).json({ message: "Register endpoint not implemented yet" });
