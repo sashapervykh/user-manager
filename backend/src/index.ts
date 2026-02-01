2b0f372bffa004a0d75f4afb11b1e9e76283edad
Aleksandr Pervykh <alekspervykh2230521@gmail.com>
2026-02-01 19:44:12 +0300

    feat: add basic users route

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,11 +1,14 @@
 import express from "express";
 import { handleErrors } from "@middlewares/handleErrors.js";
 import authRouter from "@routes/auth.route.js";
+import { requireAuth } from "@middlewares/requireAuth.js";
 
 const port = process.env.PORT ?? 3000;
 const app = express();
 app.use(express.json());
+app.use("/users", authRouter);
 app.use("/auth", authRouter);
+app.get("/users", requireAuth, (req, res) => res.status(200).send("All good"));
 app.use(handleErrors);
 app.get("/", (req, res) => {
   res.send("Hello World!");
