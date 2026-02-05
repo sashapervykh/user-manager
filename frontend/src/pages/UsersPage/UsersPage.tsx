10b4fdb2143cbb196619053ba0b3969dd26b1261
Aleksandr Pervykh <alekspervykh2230521@gmail.com>
2026-02-05 12:03:23 +0300

    refactor: extract users table in a separate component

--- frontend/src/pages/UsersPage/UsersPage.tsx
+++ frontend/src/pages/UsersPage/UsersPage.tsx
@@ -1,50 +1,10 @@
-import React, { useEffect, useState } from "react";
-import { Table } from "antd";
-import type { TableColumnsType, TableProps } from "antd";
 import Title from "antd/es/typography/Title";
 import { useUser } from "../../entities/user/model/useUser";
-import { useUsersList } from "../../features/users/model/useUsersList";
-import type { TableUser } from "../../features/users/model/types/TableUser";
 import { UsersControls } from "../../widgets/UserControls/ui/UserControls";
-
-const columns: TableColumnsType<TableUser> = [
-  {
-    title: "Name",
-    dataIndex: "name",
-  },
-  {
-    title: "Job",
-    dataIndex: "job",
-  },
-  {
-    title: "Email",
-    dataIndex: "email",
-  },
-  {
-    title: "Status",
-    dataIndex: "status",
-  },
-  {
-    title: "Last Seen",
-    dataIndex: "lastLoginAt",
-  },
-];
+import { UsersTable } from "../../widgets/usersTable/ui/UsersTable";
 
 export function UsersPage() {
   const { user } = useUser();
-  const { users, getUsers, blockUsers } = useUsersList();
-  const [chosenUsers, setChosenUsers] = useState<React.Key[]>([]);
-
-  const rowSelection: TableProps<TableUser>["rowSelection"] = {
-    onChange: (selectedRowKeys: React.Key[]) => {
-      setChosenUsers(selectedRowKeys);
-    },
-  };
-
-  useEffect(() => {
-    getUsers();
-  }, []);
-
   return (
     <>
       <div className="d-flex flex-column w-100 h-100">
@@ -55,14 +15,7 @@ export function UsersPage() {
           Let's manage some users!
         </Title>
         <UsersControls />
-        <div>
-          <Table<TableUser>
-            rowSelection={{ type: "checkbox", ...rowSelection }}
-            columns={columns}
-            dataSource={users}
-            size="large"
-          />
-        </div>
+        <UsersTable />
       </div>
     </>
   );
