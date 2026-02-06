e237ac7cec38aa3250da3ca111e307faf9359a63
Aleksandr Pervykh <alekspervykh2230521@gmail.com>
2026-02-06 18:13:41 +0300

    Revert "feat: use memo to avoid rerendering"
    This reverts commit dd38dcad2dbef6e9e7f577370a9db8e8cf7b222f.

--- frontend/src/pages/VerificationPage/VerificationPage.tsx
+++ frontend/src/pages/VerificationPage/VerificationPage.tsx
@@ -1,10 +1,10 @@
 import Title from "antd/es/typography/Title";
 import { useSearchParams } from "react-router-dom";
-import { memo, useEffect, useRef, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { apiClient } from "../../shared/api/apiClient";
 import { API_ROUTES } from "../../shared/api/apiRoutes";
 
-export const VerificationPage = memo(function VerificationPage() {
+export function VerificationPage() {
   const [searchParams, _] = useSearchParams();
   const token = searchParams.get("token");
   const [result, setResult] = useState("Verifying your email...");
@@ -34,4 +34,4 @@ export const VerificationPage = memo(function VerificationPage() {
       </div>
     </>
   );
-});
+}
